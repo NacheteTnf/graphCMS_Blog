7b1991429e7db859e80f10cee141feac2aa5a7af
Nachete <ignaciocabrerahdez97@gmail.com>
2022-11-11 23:25:55 +0000

    meta tags SEO

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <title>Nachete Crypto-Invest</title>
         <Head>
-          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
+          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" />  */}
+          <meta name="Content-Type" content="text/html"/>
+          <meta charset="UTF-8"/>
+          <meta name="google" content="nositelinkssearchbox" />
+          <meta name="google" content="notranslate" />
+          <meta name="robots" content="noindex,nofollow"/>
+          <meta name="description" content="Author: Nachete, Illustrator: CryptoInvest, Category: Crypto, Invest, Golden Way, Kannabyte, Bitcoin"/>
+          <meta name='title' content='Golden Way, Crypto Invest, Earn Money Fast, Bitcoin' />
         </Head>
         <body>
           <Main />
